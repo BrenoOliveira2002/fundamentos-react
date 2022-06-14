a120395c4fbb87f4c40899de6e604019175559da
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-14 12:53:19 -0300

    component with property

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './componentes/basicos/Primeiro'
+import ComParametros from './componentes/ComParametros'
 
 ReactDOM.render(
 
     <div> 
       <Primeiro> </Primeiro> 
+      <ComParametros
+       titulo="Situacao do aluno"
+       aluno="Breno"
+       nota="9.3">
+        
+       </ComParametros>
     </div>,
      document.getElementById('root')
 )
