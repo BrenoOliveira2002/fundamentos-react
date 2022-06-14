6e4ac461a0077eb368324b29dbfe570faddf7dd0
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-14 12:19:57 -0300

    first component #2

--- src/componentes/basicos/Primeiro.js
+++ src/componentes/basicos/Primeiro.js
@@ -1,6 +1,13 @@
+import React from 'react'
 
 export default function Primeiro() {
 
-    return 'Primeiro Componente'
+    const msg = 'Seja bem vindo(a)!'
 
+    return (
+    <div>
+         <h2> Primeiro componente</h2>
+         <p> { msg }</p>
+    </div>
+    )
 }
