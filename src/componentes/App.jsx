cf2beb1e25bb21c7301acde1fbca2053db6cdab0
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-15 09:50:01 -0300

    card component

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-
+import Card from './basicos/Layout/Card'
 import Aleatorio from './basicos/Aleatorio'
 import Primeiro from './basicos/Primeiro'
 import ComParametros from './basicos/ComParametros'
@@ -8,18 +8,35 @@ import Fragmentos from './basicos/Fragmentos'
 
 
 
-export default () => (  
+export default () => (
 
-    <div id="app">
-      <h1>Fundamentos React </h1>
+  <div id="app">
+    <h1>Fundamentos React </h1>
+
+    <Card titulo="#4Desafio Aleatorio">
+
       <Aleatorio min={1} max={60} />
-      <Fragmentos />
+
+    </Card>
+
+    <Card titulo="#3Fragmento">
+
+      <Fragmentos> </Fragmentos>
+
+    </Card>
+
+    <Card titulo="#2Com Parametro">
+
       <ComParametros
         titulo="Situacao do aluno"
         aluno="Breno"
         nota={9.3}
       />
+    </Card>
 
-      <Primeiro> </Primeiro>
-    </div>
-    );
+    <Card titulo=" #1Primeiro Componente">
+      <Primeiro></Primeiro>
+    </Card>
+
+  </div>
+);
