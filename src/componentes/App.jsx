6843e9799e8e69b63fa107f14d162e0377935277
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-06-15 09:00:11 -0300

    Random challenger

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,15 +1,18 @@
 
 import React from 'react'
+
+import Aleatorio from './basicos/Aleatorio'
 import Primeiro from './basicos/Primeiro'
 import ComParametros from './basicos/ComParametros'
 import Fragmentos from './basicos/Fragmentos'
 
 
 
-export default () => (
+export default () => (  
 
     <div id="app">
-      <h1>Fundamentos React (Arrow _)</h1>
+      <h1>Fundamentos React </h1>
+      <Aleatorio min={1} max={60} />
       <Fragmentos />
       <ComParametros
         titulo="Situacao do aluno"
